fd6265721275000e1e6d1e32865164fe85bfbd09
cicduser2022 <113808829+cicduser2022@users.noreply.github.com>
2022-10-13 00:30:57 -0300

    ajuste

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "vnet-remote-state2" {
   bucket = "my-tf-test-bucket-micky-19287319837892137"
-  acl    = "private"
+  acl    = "public"
 
   tags = {
     Name        = "My bucket"
